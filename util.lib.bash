6339ff73bc7b6d68e3f7708b64abc0e2c36cf622
Kelly Lauren-Summer Becker-Neuding <kbecker@kellybecker.me>
2015-07-04 22:57:58 -0700

    Added AddFile Utility Function

--- util.lib.bash
+++ util.lib.bash
@@ -5,21 +5,27 @@
 #
 
 function UtilToUpper() {
-  awk '{print toupper($0)}' <<<"$@"
+	awk '{print toupper($0)}' <<<"$@"
 }
 
 function UtilToLower() {
-  awk '{print tolower($0)}' <<<"$@"
+	awk '{print tolower($0)}' <<<"$@"
 }
 
 function UtilConstantize() {
-  UtilToUpper "$@" | sed 's/[^A-Z]+/_/g' | UtilToLower
+	UtilToUpper "$@" | sed 's/[^A-Z]+/_/g' | UtilToLower
 }
 
 function UtilDasherize() {
-  UtilToUpper "$@" | sed 's/[^A-Z]+/-/g' | UtilToLower
+	UtilToUpper "$@" | sed 's/[^A-Z]+/-/g' | UtilToLower
 }
 
 function UtilCountLines() {
-  wc -l <<<"$1"
+	wc -l <<<"$1"
+}
+
+function AddFile() {
+	File "AddFile" "$2" <<-EOF
+  $(cat "$1")
+	EOF
 }
